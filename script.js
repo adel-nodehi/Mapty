ae7cfe25addd95ff7d7b12adf82683330d43b2c2
Adel <adelnmo2003@gmail.com>
2024-05-05 20:45:19 +0330

    Additional Feature ideas

--- script.js
+++ script.js
@@ -313,3 +313,18 @@ class App {
 }
 
 const app = new App();
+
+/**
+ * Additional Feature ideas:
+ *
+ * 1. Ability to edit a workout;
+ * 2. Ability to delete a workout;
+ * 3. Ability to delete all workouts;
+ * 4. Ability to sort workouts by a certain field (e.g. distance);
+ * 5. Re-build Running and Cycling objects coming from Local Storage;
+ * 6. More realistic error and confirmation messages;
+ * 7. Ability to position the map to show all workouts [very hard];
+ * 8. Ability to draw lines and shapes instead of just points [very hard];
+ * 9. Geocode location from coordinates (“Run in Faro, Portugal”) [only after asynchronous JavaScript section];
+ * 10. Display weather data for workout time and place [only after asynchronous JavaScript section].
+ */
